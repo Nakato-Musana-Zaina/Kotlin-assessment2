82326aa5ffeea566a6c4186da1baa10108dda47c
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-03-25 08:27:49 +0300

    added work

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+    integer(arrayOf(2,6,4,6))
+
+    sphere(8)
+
+    buns("Barnie bakes brwon bagels and buns")
+
+
+
+}
+
+
+
+fun integer(num: Array<Int>):Int{
+   var sums = num.sum()
+    println(sums)
+    return sums
+
+    var counts = num.count()
+    println(counts)
+    return counts
+
+     var average = sums/counts
+    println(average)
+    return average
+
+
+
+}
+
+fun sphere(num1:Int):Double{
+    var vol =4/3
+    var prod = vol* 3.14159
+    var radius = num1*num1*num1
+    var volume = prod*radius
+    println(volume)
+    return volume
+
+}
+
+//fun ispalindrome(word:String):Boolean{
+ //   if()
+//}
+
+fun buns(sentences:String) {
+    var spliting = sentences.lowercase()
+    var b = spliting.replace("b"," ")
+    println(b)
+}
